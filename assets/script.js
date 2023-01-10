fe27d9858787d50985258e79cf78b47457c0a57a
abbyd <abbydiaz300@gmail.com>
2023-01-10 15:30:43 -0800

    added js to go with added html to be semi-functional

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
-// 4.1.8 add items with click button if confused 
-var buttonEl = document.querySelector('#save-task');
+// 4.1.8 add items with click (event listener) button if confused 
+var formEl = document.querySelector('#task-form');
 var tasksToDoEl = document.querySelector('#tasks-to-do');
 
-var createTaskHandler = function () {
+var createTaskHandler = function (event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement('li');
     listItemEl.className = 'task-item';
     listItemEl.textContent = ' u have more shit to do ';
     tasksToDoEl.appendChild(listItemEl);
+
+    console.log(event);
 };
 
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
